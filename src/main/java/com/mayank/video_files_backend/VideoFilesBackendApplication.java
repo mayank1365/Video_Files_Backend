83da16e1d741d4134d409849be979d4433918d21
Mayank <mayank1365@gmail.com>
2024-07-13 12:59:41 +0530

    initial commit

---
+++ src/main/java/com/mayank/video_files_backend/VideoFilesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.video_files_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoFilesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoFilesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mayank/video_files_backend/VideoFilesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.video_files_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoFilesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
